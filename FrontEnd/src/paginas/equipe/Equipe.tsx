24b969e07857dcaa0c3af667e01868a334f3ee4e
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-09-07 21:32:41 -0300

    add pag equipe

--- FrontEnd/src/paginas/equipe/Equipe.tsx
+++ FrontEnd/src/paginas/equipe/Equipe.tsx
@@ -15,150 +15,149 @@ export default function Equipe() {
                 <Typography variant="h4" color="initial" className='titulo-equipe'>Nossa Equipe</Typography>
             </Grid>
             <Grid className='container-equipe'>
+                
+
                 <Card className='cartao'>
                     <CardMedia
                         component="img"
                         height="200px"
-                        image="https://i.imgur.com/qS1Mn8z.jpg"
-                        alt="deivide"
+                        image="https://avatars.githubusercontent.com/u/89025317?v=4"
+                        alt="nome"
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div" className="nome">
-                            Deivide Joaquim
+                            Samuel Pereira Brandão
                         </Typography>
                         <Typography variant="body2" color="text.secondary" className="texto-card">
-                            Prazer, tenho 27 anos e sou de Guarulhos/SP. Eu amo tecnologia e meu hooby preferido é estar com a minha familia.
+                        Sempre codifique como se o cara que acabasse mantendo o seu código fosse um psicopata violento que sabe onde você mora.
                         </Typography>
                     </CardContent>
 
                     <CardActions className="botao-card" >
                         <Box mx={1}>
-                            <a href="https://github.com/Dejsilva" target="_blank" className='text-decorator-none'>
+                            <a href="https://github.com/SamuelPereiraBrandao" target="_blank" className='text-decorator-none'>
                                 <GitHubIcon className='git' />
                             </a>
                         </Box>
                         <Box mx={1}>
-                            <a href="https://www.linkedin.com/in/deivide-silva-748175213/" target="_blank" className='text-decorator-none'>
+                            <a href="https://www.linkedin.com/in/samuelpereirabrandao/" target="_blank" className='text-decorator-none'>
+                                <LinkedInIcon className='linkedin' />
+                            </a>
+                        </Box>
+                        <br /> <br />
+                    </CardActions>
+                </Card>
+                <Card className='cartao'>
+                    <CardMedia
+                        component="img"
+                        height="200px"
+                        image="https://avatars.githubusercontent.com/u/85175391?v=4"
+                        alt="nome"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div" className="nome">
+                        Guilherme Rodrigues dos Santos
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" className="texto-card">
+                        Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.
+                        </Typography>
+                    </CardContent>
+
+                    <CardActions className="botao-card" >
+                        <Box mx={1}>
+                            <a href="https://github.com/GuilhermeRodriguesSantos" target="_blank" className='text-decorator-none'>
+                                <GitHubIcon className='git' />
+                            </a>
+                        </Box>
+                        <Box mx={1}>
+                            <a href="https://www.linkedin.com/in/guilhermedev/" target="_blank" className='text-decorator-none'>
                                 <LinkedInIcon className='linkedin' />
                             </a>
                         </Box>
                     </CardActions>
                 </Card>
-
                 <Card className='cartao'>
                     <CardMedia
                         component="img"
                         height="200px"
-                        image="https://i.imgur.com/S0EGvAp.jpg"
-                        alt="jeffrey"
+                        image=""
+                        alt="nome"
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div" className="nome">
-                            Jeffrey Rodrigues
+                            Nome aqui
                         </Typography>
                         <Typography variant="body2" color="text.secondary" className="texto-card">
-                            Prazer, tenho 29 anos. Sou entusiasta da tecnologia, amo viajar, superar desafios e jogar videogame nas horas vagas.
+                            Texto aqui
                         </Typography>
                     </CardContent>
 
                     <CardActions className="botao-card" >
                         <Box mx={1}>
-                            <a href="https://github.com/JeffreyRodriigues" target="_blank" className='text-decorator-none'>
+                            <a href="#" target="_blank" className='text-decorator-none'>
                                 <GitHubIcon className='git' />
                             </a>
                         </Box>
                         <Box mx={1}>
-                            <a href="https://www.linkedin.com/in/jeffreyrodrigues/" target="_blank" className='text-decorator-none'>
+                            <a href="#" target="_blank" className='text-decorator-none'>
                                 <LinkedInIcon className='linkedin' />
                             </a>
                         </Box>
                     </CardActions>
                 </Card>
-
                 <Card className='cartao'>
                     <CardMedia
                         component="img"
                         height="200px"
-                        image="https://i.imgur.com/YNWcMQq.jpg"
-                        alt="gabriel"
+                        image=""
+                        alt="nome"
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div" className="nome">
-                            Gabriel Mendes
+                            Nome aqui
                         </Typography>
                         <Typography variant="body2" color="text.secondary" className="texto-card">
-                            Eaee, tenho 19 anos sou de São Paulo/SP. Sou uma pessoa muito persistente e apaixonado por Tecnologia da Informação.
+                            Texto aqui
                         </Typography>
                     </CardContent>
 
                     <CardActions className="botao-card" >
                         <Box mx={1}>
-                            <a href="https://github.com/Biellms" target="_blank" className='text-decorator-none'>
+                            <a href="#" target="_blank" className='text-decorator-none'>
                                 <GitHubIcon className='git' />
                             </a>
                         </Box>
                         <Box mx={1}>
-                            <a href="https://www.linkedin.com/in/gabriel-mendes-0706ab1b8/" target="_blank" className='text-decorator-none'>
+                            <a href="#" target="_blank" className='text-decorator-none'>
                                 <LinkedInIcon className='linkedin' />
                             </a>
                         </Box>
                     </CardActions>
                 </Card>
-
                 <Card className='cartao'>
                     <CardMedia
                         component="img"
                         height="200px"
-                        image="https://i.imgur.com/yLO6JBC.jpg"
-                        alt="lucas"
+                        image=""
+                        alt="nome"
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div" className="nome">
-                            Lucas Bernardino
+                            Nome aqui
                         </Typography>
                         <Typography variant="body2" color="text.secondary" className="texto-card">
-                             Olá prazer, tenho 19 anos e moro em São Paulo/SP. E sou apaixonado pelo universo.
+                            Texto aqui
                         </Typography>
                     </CardContent>
 
                     <CardActions className="botao-card" >
                         <Box mx={1}>
-                            <a href="https://github.com/LucasIluminado" target="_blank" className='text-decorator-none'>
+                            <a href="#" target="_blank" className='text-decorator-none'>
                                 <GitHubIcon className='git' />
                             </a>
                         </Box>
                         <Box mx={1}>
-                            <a href="https://www.linkedin.com/in/lucasdevsousa/" target="_blank" className='text-decorator-none'>
-                                <LinkedInIcon className='linkedin' />
-                            </a>
-                        </Box>
-                    </CardActions>
-                </Card>
-
-                <Card className='cartao'>
-                    <CardMedia
-                        component="img"
-                        height="200px"
-                        image="https://i.imgur.com/Uubnybs.png"
-                        alt="milton"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div" className="nome">
-                            Milton Viegas
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" className="texto-card">
-                            Africano bacana de 30 anos. Autodidata, extremamente curioso e viajante.
-                        </Typography>
-                    </CardContent>
-
-                    <CardActions className="botao-card" >
-                        <Box mx={1}>
-                            <a href="https://github.com/Milton-Viegas" target="_blank" className='text-decorator-none'>
-                                <GitHubIcon className='git' />
-                            </a>
-                        </Box>
-                        <Box mx={1}>
-                            <a href="https://www.linkedin.com/in/milton-viegas-849655b0/" target="_blank" className='text-decorator-none'>
+                            <a href="#" target="_blank" className='text-decorator-none'>
                                 <LinkedInIcon className='linkedin' />
                             </a>
                         </Box>
