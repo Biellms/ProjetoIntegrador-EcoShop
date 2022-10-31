dcce669cf6ea349769789b0bffe6dd565eb126c9
Biellms <biell.mendes8@gmail.com>
2022-10-31 11:43:27 -0300

    Upd section 3 home

--- FrontEnd/ecoshop/src/pages/home/AccordionHome.jsx
+++ FrontEnd/ecoshop/src/pages/home/AccordionHome.jsx
@@ -8,66 +8,74 @@ import './AccordionHome.css';
 
 export const AccordionHome = () => {
 
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return (
         <>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-                    <Typography>Accordion 1</Typography>
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
+                    <span className='home-span'>
+                        Proteção do meio ambiente
+                    </span>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
+                    <p className='home-p-accordion'>
+                        O primeiro fator que demonstra a relevância da sustentabilidade no e-commerce é a contribuição com a preservação do meio ambiente.
+                        Trata-se de um modelo de negócio responsável e que auxilia a evitar desastres ambientais e impactos nos ambientes naturais.
+                    </p>
                 </AccordionDetails>
             </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2a-content"
-                    id="panel2a-header"
-                >
-                    <Typography>Accordion 2</Typography>
+            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel4bh-content" id="panel4bh-header">
+                    <span className='home-span'>
+                        Tripé da Sustentabilidade
+                    </span>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
+                <p className='home-p-accordion'>
+                        O chamado tripé da sustentabilidade é baseado em três princípios: o social, o ambiental e o econômico.
+                        Esses três fatores precisam ser integrados para que a sustentabilidade de fato aconteça. Sem eles, a sustentabilidade não se sustenta.
+                        <br /><br />
+                        <b>• Social:</b> Engloba as pessoas e suas condições de vida, como educação, saúde, violência, lazer, dentre outros aspectos.<br />
+                        <b>• Ambiental:</b> Refere-se aos recursos naturais do planeta e a forma como são utilizados pela sociedade, comunidades ou empresas.<br />
+                        <b>• Econômico:</b> Relacionado com a produção, distribuição e consumo de bens e serviços. A economia deve considerar a questão social e ambiental.<br />
+                    </p>
                 </AccordionDetails>
             </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2a-content"
-                    id="panel2a-header"
-                >
-                    <Typography>Accordion 3</Typography>
+            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel2bh-content" id="panel2bh-header">
+                    <span className='home-span'>
+                        Uso de materiais recicláveis
+                    </span>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
+                <p className='home-p-accordion'>
+                        Os materiais recicláveis são essenciais para reduzir a quantidade de resíduos destinados de forma irregular e ainda gerar renda à sociedade.
+                        Portanto, é preciso repensar os processos e otimizar o uso desses materiais, sempre garantindo que sejam enviados para a reciclagem, e não para aterros sanitários.
+                    </p>
                 </AccordionDetails>
             </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2a-content"
-                    id="panel2a-header"
-                >
-                    <Typography>Accordion 4</Typography>
+            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel3bh-content" id="panel3bh-header">
+                    <span className='home-span'>
+                        Processos produtivos mais sustentáveis
+                    </span>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
+                <p className='home-p-accordion'>
+                        Os processos produtivos são os principais alvos para implementar ações sustentáveis, evitando excessos, desperdícios e consumo desordenado dos recursos naturais.
+                        Portanto, se o e-commerce conta com a produção, é um setor que deve ser revisado, principalmente considerando os seguintes aspectos:
+                        <br /><br />
+                        • Uso de matéria-prima sustentável;<br />
+                        • Consumo consciente de energia;<br />
+                        • Economia ou reutilização da água;<br />
+                        • Destinação adequada dos resíduos;<br />
+                        • Estratégias de educação ambiental.
+                    </p>
                 </AccordionDetails>
             </Accordion>
         </>
